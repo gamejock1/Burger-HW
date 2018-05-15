cf1ae86aff114a55aec04a14e22bcd675daff1e1
John yeager <johnyeager@johns-mbp.home>
2018-05-15 11:06:03 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured)
+values ('Triple Bypass', false), ('The Kitchen Sink', false), ('The Royal Burger', false),
+('The Triple', true), ('The Classic', true), ('Jerk Burger', false);
