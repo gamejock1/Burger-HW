1c13e53bc2529f03619aa094584b3bb590ae653e
John yeager <johnyeager@Johns-MacBook-Pro.local>
2018-05-17 19:41:34 -0400

    added functionality

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,29 @@
-
+var connection = require("./connection.js");
 
 
 var orm = {
   selectAll: function(cb){
-    connection.query("SELECT * FROM burgers", tableName, function(err, res){
-      cb(json(res))
+    connection.query("SELECT * FROM burgers", function(err, res){
+      cb(res);
     })
   },
   insertOne: function(burgerName, devoured){
-    connection.query("INSERT INTO burgers VALUES (?,?)", [burgerName, devoured], function(err, res){
-    console.log('burger added');
+    connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?,?)", [burgerName, devoured], function(err, res){
+    // console.log(data);
     })
   },
   updateOne: function(burgerName, oldBurger){
     var querystring = "UPDATE burgers SET burger_name = ?, devoured = 0 WHERE burger_name = ?";
     connection.query(querystring, [burgerName, oldBurger], function(err, res){
-    console.log('burger added');
+    console.log('burger updated');
     })
+  },
+  deleteOne: function(dataId){
+    connection.query("DELETE FROM burgers WHERE id = ?", dataId, function(err, res){
+      console.log('Burger deleted!');
+      })
   }
 
 };
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -7,3 +7,20 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// var apiRoutes = require("./controllers/burgers_controller");
+// apiRoutes(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
